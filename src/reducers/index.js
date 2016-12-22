a110f8de23fce2f0513f777abaffc1dce61451d5
VV <huhushirley@163.com>
2016-12-22 16:36:20 +0800

    remove unnecessary files

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,5 @@
 import { routerReducer } from 'react-router-redux';
 import { combineReducers } from 'redux';
-import merge from 'lodash.merge';
 import errorMessage from './errorMessage';
 import entities from './entities';
 import counter from './counter';
